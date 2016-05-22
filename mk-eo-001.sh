b7594ce01fa3ae396ef7dc724e195d28d3a1f6c9
mykespb <mykespb@gmail.com>
2016-05-22 05:20:27 +0300

    sorting done

--- mk-eo-001.sh
+++ mk-eo-001.sh
@@ -1,3 +1,4 @@
 ./mk-eo-vortaro-2-getwords1.py > ./mk-eo-vortaro-2-getwords1.txt
 ./mk-eo-vortaro-2-getwords2.py > ./mk-eo-vortaro-2-getwords2.txt
 ./mk-eo-vortaro-2-getwords3.py > ./mk-eo-vortaro-2-getwords3.txt
+sort eo-vortaro-2-list3.txt >eo-vortaro-2-list3s.txt
