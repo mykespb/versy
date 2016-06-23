2c49a5d527b8e4e473775adf7e7c70ed002b5bb8
mykespb <mykespb@gmail.com>
2016-06-24 00:11:10 +0300

    tools and test upwith full list of preps

--- mk-eo-paras1.py
+++ mk-eo-paras1.py
@@ -7,9 +7,23 @@
 
 from mk_eo_tools import *
 
+txt1 = "en la mondon venis nova sento"
+txt2 = "tri sonoj ŝxajnis al mi"
+
+txts = [txt1, txt2]
+
+def test1(f):
+    """ test given phrase """
+    for txt in f:
+        print ("\ndonata:  ", txt)
+        s = eo_split(txt)
+        st = [base_form(x) for x in s]
+        print ("rezulta: ", st)
+
 def main(args):
     print ("numeraloj: ", EO_NUMS)
     print ("pronomoj: ", EO_PRONOMOJ)
+    test1(txts)
     return 0
 
 if __name__ == '__main__':
--- mk_eo_tools.py
+++ mk_eo_tools.py
@@ -1,14 +1,18 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 # mk-eo-tools.py
-# 2016-06-23 0.1
+# 2016-06-23 1.1
 # toolbase for SSYP-2016 eo
+
+#~ import re
 
 ALFA_EO_UP = "ABCĈDEFGĜHĤIJĴKLMNOPRSŜTUŬVZ"
 ALFA_EO_DN = "abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
+ALFA_EO    = ALFA_EO_UP + ALFA_EO_DN
 
 ALFA_EN_UP = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
 ALFA_EN_DN = "abcdefghijklmnopqrstuvwxyz"
+ALFA_EN    = ALFA_EN_UP + ALFA_EN_DN
 
 EO_VOVELOJ = "aeiou"
 
@@ -18,6 +22,58 @@ EO_PRONOMOJ_S = "mi ci li ŝi ŝli ĝi oni".split()
 EO_PRONOMOJ_P = "ni vi ili".split()
 EO_PRONOMOJ = EO_PRONOMOJ_P + EO_PRONOMOJ_S
 
+EO_ART = ["la"]
+
+EO_VERBFIN = "i is as os us u".split()
+
+EO_PREP = "al apud ĉe ĉirkaŭ da de dum en el ekster for inter krom kun per por post preter pri sen sub super sur tra trans".split()
+
+EO_UNIO = "kaj se sed aŭ ju nek des".split()
+
+EO_PREFIX = "eks dis mal mis ek re fi bo ge pra antaŭ kontraŭ".split()
+
+EO_STOP = EO_PREP + EO_UNIO + EO_PREFIX + EO_ART
+
 def base_form (w):
     """ finds base form for given word """
+
+    if w in EO_NUMS or w in EO_PRONOMOJ or w in EO_STOP:
+        return w
+
+    if w.endswith("on") or w.endswith("ojn") or w.endswith("an") or w.endswith("ajn"):
+        w = w[:-1]
+
+    if w.endswith("j"):
+        w = w[:-1]
+
+    if w.endswith("'"):
+        w = w[:-1] + "o"
+
+    if w.endswith("o") or w.endswith("a"):
+        return w
+
+    for e in EO_VERBFIN:
+        if w.endswith(e):
+            w = w[:-len(e)] + "i"
     return w
+
+    return w
+
+def eo_split(f):
+    """ split phrase en eo into words"""
+
+    f1 = f.split()
+    f2 = [eo_strim(x) for x in f1]
+
+    return f2
+
+def eo_strim(w):
+    """ delete all non-letter chars from word"""
+
+    s = ""
+    for c in w or c in "'-":
+        if c in ALFA_EO:
+            s += c
+
+    return s
+
